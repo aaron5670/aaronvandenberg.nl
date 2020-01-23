eb25720c1483d9a48e54f5b6ca2f5664f81209a2
Aaron van den Berg <a.vdberg98@gmail.com>
2020-01-23 22:10:08 +0100

    :speech_balloon: Updated alt text

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -10,7 +10,7 @@ export const Contact = () => (
       <ContactForm />
     </Details>
     <Thumbnail>
-      <img src={contact} alt="I’m John and I’m a Backend & Devops engineer!" />
+      <img src={contact} alt="I’m Aaron and I’m a student Web Developer!" />
     </Thumbnail>
   </Wrapper>
 )
--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -17,7 +17,7 @@ export const Intro = () => (
 				</Button>
 			</Details>
 			<Thumbnail>
-				<img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+				<img src={dev} alt="Aaron van den Berg" />
 			</Thumbnail>
 		</IntroWrapper>
 	</Wrapper>
