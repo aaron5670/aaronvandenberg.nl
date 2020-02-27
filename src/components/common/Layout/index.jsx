5853929326b51da61ceb30bbfacc1d769e2a4815
Aaron van den Berg <a.vdberg98@gmail.com>
2020-02-27 20:13:47 +0100

    :fire: Removed old unused websocket code & component

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import {openWebSocket} from "../../../services/socket";
 import {Footer} from 'components/theme'
 import {Global} from './styles'
 import './fonts.css'
 
 export const Layout = ({children}) => {
-    openWebSocket();
     return (
         <>
             <Global/>
